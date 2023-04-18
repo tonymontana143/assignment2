8e097b5fe967b7bfac9d0a4bb27da0da671da478
TonyMontana143 <221074@astanait.edu.kz>
2023-04-18 19:25:59 +0600

    Implemented "add" method to add element if size is enough

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,6 +1,31 @@
 import java.util.Arrays;
 public class MyArrayList implements MyList<T>{
-    private Object[] array;
+    private Object[] elements;
     private int size;
+    public MyArrayList(){
+        elements=new Object[10];
+        size=0;
+    }
+    @Override
+    public int getSize(){
+        return size;
+    }
+    @Override
+    public boolean contains(Object o){
+        return indexOf(o)!=-1;
+    }
+    private void ensureCapacity(int minCapacity) {
+        if (minCapacity > elements.length) {
+            int newCapacity = elements.length * 2;
+            if (newCapacity < minCapacity) {
+                newCapacity = minCapacity;
+            }
+            elements = Arrays.copyOf(elements, newCapacity);
+        }
+    @Override
+    public void add(T item){
+        ensureCapacity(size+1);
+        elements[size++]=item;
+    }
 
 }
