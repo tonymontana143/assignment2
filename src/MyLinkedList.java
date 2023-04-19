d5f98dfd9a8f010e40e29b501704b062d2492704
TonyMontana143 <221074@astanait.edu.kz>
2023-04-19 20:56:36 +0600

    Private inner class "Node" defined and references to next and previous nodes

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,2 +1,66 @@
 public class MyLinkedList<T> implements MyList<T>{
+    private class Node<T>{
+        public T elements;
+        public Node<T> next;
+        public Node<T> previous;
+        public Node(T elements){
+            this.elements=elements;
+            this.next=null;
+            this.previous= null;
+        }
+    }
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public void add(T item) {
+
+    }
+
+    @Override
+    public void add(T item, int index) {
+
+    }
+
+    @Override
+    public boolean remove(T item) {
+        return false;
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
 }
